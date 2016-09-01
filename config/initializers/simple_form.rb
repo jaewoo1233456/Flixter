12d1eb54cb43481d5e15461b0eb0031d6d94c4b4
Jaewoo Park <jp3242@stern.nyu.edu>
2016-09-01 21:32:41 +0000

    Built a new form page

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -16,7 +16,7 @@ SimpleForm.setup do |config|
 
     # Determines whether to use HTML5 (:email, :url, ...)
     # and required attributes
-  b.use :html
+  b.use :html5
 
     # Calculates placeholders automatically from I18n
     # You can also pass a string as f.input placeholder: "Placeholder"
